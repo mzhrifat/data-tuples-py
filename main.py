6db970b742cc5656a9e373eb0ab340cc020a5e57
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2024-12-17 01:58:31 +0600

    second commit

--- main.py
+++ main.py
@@ -4,9 +4,24 @@ Tuples
 -Allow duplicates
 -Heterogeneous
 -fIXED sizes
+_immutable data type
+
+tuple methods
+-count():return no.of occurences TIMES A specified value appars in the tuple
+-index()
 """
 
 
-
+""""
 fruits=("apple","banana","cherry",1,True)
+print(id(fruits))
 print(fruits)
+"""
+"""
+numbers=(1,2,3,4,5)
+valueCount=numbers.count(1)
+print(valueCount)
+"""
+numbers=(1,2,3,2,4,2)
+valueIndex=numbers.index(3)
+print(valueIndex)
